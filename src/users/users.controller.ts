7704643bfb42f8b6b3421aaad7a5d5375facdbcb
maksymtymchenko <makstimchenk@gmail.com>
2025-03-03 21:58:09 +0200

    fix user id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
@@ -11,8 +11,8 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  @UseGuards(JwtAuthGuard)
-  getMe(@UserId() userId: number) {
-    return this.usersService.findById(userId);
+  getMe(@UserId() userId: number, @Query('id') queryId: string) {
+    const id = queryId ? parseInt(queryId) : userId;
+    return this.usersService.findById(id);
   }
 }
